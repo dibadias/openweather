745b17c1c6f218f90b299478c9e21532a9e2cbad
Diego Batista Dias Leite <dibadias@Diegos-MacBook-Pro.local>
2017-06-24 00:10:07 -0300

    Add Login View Controller, Facebook Framework and images

---
+++ CIANDTTest/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  CIANDTTest
+//
+//  Created by Diego Batista Dias Leite on 23/06/17.
+//  Copyright © 2017 Diego Batista Dias Leite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ CIANDTTest/LoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.m
+//  CIANDTTest
+//
+//  Created by Diego Batista Dias Leite on 23/06/17.
+//  Copyright © 2017 Diego Batista Dias Leite. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
